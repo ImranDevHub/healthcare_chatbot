91edfdc623769a854e39164d2f48192c1a16c8da
Imran Muhammad <imrandevhub@gmail.com>
2025-10-08 16:33:40 +0300

    feat: add dotenv configuration and server start scripts

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,4 +1,16 @@
 import express from "express";
+import type { Request, Response } from "express";
+import dotenv from "dotenv";
 
-const app = express()
-const port = process.env.PORT
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 300;
+
+app.get('/', (req:Request, res:Response) => { 
+    res.send('Hello World!');
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`)
+});
