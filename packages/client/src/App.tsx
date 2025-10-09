e4c4a2f9d1649cd1f6e8a3a76bf7047a827fdc0d
Imran Muhammad <imrandevhub@gmail.com>
2025-10-09 14:48:56 +0300

    feat: setup Husky and prettier

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -3,21 +3,21 @@ import './App.css';
 import { Button } from './components/ui/button';
 
 function App() {
-  const [message, setMessage] = useState('');
+    const [message, setMessage] = useState('');
 
-  useEffect(() => {
-    fetch('/api/hello')
-      .then(response => response.json())
-      .then(data => setMessage(data.message))
-      .catch(error => console.error('Error fetching message:', error));
-  }, []);
+    useEffect(() => {
+        fetch('/api/hello')
+            .then((response) => response.json())
+            .then((data) => setMessage(data.message))
+            .catch((error) => console.error('Error fetching message:', error));
+    }, []);
 
-  return (
-    <>
-      <p className="font-bold">{message || 'Loading...'}</p>
-      <Button>Submit</Button>
-    </>
-  );
+    return (
+        <>
+            <p className="font-bold">{message || 'Loading...'}</p>
+            <Button>Submit</Button>
+        </>
+    );
 }
 
 export default App;
