564a6631c1af1d9e442cba0dadcd49a31ce6581e
Imran Muhammad <imrandevhub@gmail.com>
2025-10-09 12:40:59 +0300

    feat: setup shadcn/ui add utility functions, update TypeScript configuration, and implement button component

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import './App.css';
+import { Button } from './components/ui/button';
 
 function App() {
   const [message, setMessage] = useState('');
@@ -11,7 +12,12 @@ function App() {
       .catch(error => console.error('Error fetching message:', error));
   }, []);
 
-  return <p className='font-bold'>{message || 'Loading...'}</p>;
+  return (
+    <>
+      <p className="font-bold">{message || 'Loading...'}</p>
+      <Button>Submit</Button>
+    </>
+  );
 }
 
 export default App;
